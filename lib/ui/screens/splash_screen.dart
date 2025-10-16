5c67c4420ade50d1b31a9b4f33e3a60a74ba786c
faiaz5415 <faiazzhasan@gmail.com>
2025-10-16 19:55:58 +0600

    Initial commit

---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            'assets/images/background.svg',
+            width: double.maxFinite,
+            height: double.maxFinite,
+            fit: BoxFit.cover,
+          ),
+          Center(child: SvgPicture.asset('assets/images/logo.svg',
+              height: 60)),
+        ],
+      ),
+    );
+  }
+}
